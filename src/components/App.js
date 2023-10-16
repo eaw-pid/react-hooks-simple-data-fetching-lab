9409ffc046e50e2a8b1830603364823abb95bee3
Erin Wise <erinwise88@gmail.com>
2023-10-16 14:29:57 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useState, useEffect } from "react"
+import { data } from "../mocks/data";
+
+function App() {
+
+    const [dogImg, setDogImg] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => {
+       console.log(data.message)
+        setDogImg(data.message)
+        setIsLoaded(true)
+    })
+}, []);
+
+if (!isLoaded) return <p>Loading...</p>
+
+
+
+return <img src={data.message} alt="A Random Dog"/>
+
+}
+
+
+export default App
